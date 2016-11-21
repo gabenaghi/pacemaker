29c76edb42b8f3883698cb62151a3fbd943c154c
Jack Harkins <harkj@seas.upenn.edu>
2016-11-21 16:57:30 -0500

    fixed heart.cpp accidental comment-out

--- milestone2/heart/heart.cpp
+++ milestone2/heart/heart.cpp
@@ -1,11 +1,13 @@
 #include "common.h"
+#include "generator.h"
+#include "common.h"
 
 Thread threads[NUM_THREADS]; //default osPriority Normal
 
 int main()
 {
 
-   //threads[T_GENERATOR].start(generator_thread);
-   //threads[T_RESPONDER].start(responder_thread);
+   threads[T_GENERATOR].start(generator_thread);
+   threads[T_RESPONDER].start(responder_thread);
    return 0; 
 }
