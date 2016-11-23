3fe0401a97481d2fd9be039555bcd57ec3fd22d3
Jack Harkins <harkj@seas.upenn.edu>
2016-11-22 23:31:33 -0500

    fixed external signals header file

--- milestone2/pacemaker/external_signals.h
+++ milestone2/pacemaker/external_signals.h
@@ -2,10 +2,13 @@
 #ifndef EXTERNAL_SIGNALS_H
 #define EXTERNAL_SIGNALS_H
 
+void flip_led(uint8_t led);
 void vget_received(void);
 void aget_received(void);
 void reset_vpace(void);
 void reset_apace(void);
+void set_vpace(void);
+void set_apace(void);
 void external_signals_thread(void);
 
 #endif // EXTERNAL_SIGNALS_H
