bd129da34f3d4a1926b725a3bd59c8324b41232f
Gabriel Naghi <gabenaghi@seas.0661.apng.wireless-pennnet.upenn.edu>
2016-11-28 22:25:04 -0500

    put display in while loop

--- milestone2/heart/display.cpp
+++ milestone2/heart/display.cpp
@@ -10,28 +10,31 @@ void display_thread()
     float observation_time;
     display_timer.start();
     display_state state = Display;
-    
-    switch (state)
+    while(1)
     {
-        case Display:
-            switch (rand() % 2)
-            {
-                case 0:
-                    if (display_timer.read() > OBSERVATION_MIN)
-                    {
-                        observation_time = display_timer.read();
-                        lcd.printf("Acount %3.0f bpm\n Vcount: %3.0f bpm\n",(float)Acount/observation_time, (float)Vcount/observation_time);
-                        Acount = 0;
-                        Vcount = 0;
-                        display_timer.reset();
-                    }
-                    break;
-                    
-                case 1:
-                    //do nothing
-                    break;   
-            }
-            break;   
+        switch (state)
+        {
+            case Display:
+                switch (rand() % 2)
+                {
+                    case 0:
+                        if (display_timer.read() > OBSERVATION_MIN)
+                        {
+                            observation_time = display_timer.read();
+                            lcd.printf("Acount %3.0f bpm\n Vcount: %3.0f bpm\n",(float)Acount/observation_time, (float)Vcount/observation_time);
+                            Acount = 0;
+                            Vcount = 0;
+                            display_timer.reset();
+                        }
+                        break;
+                        
+                    case 1:
+                        //do nothing
+                        break;   
+                }
+                break;   
+        }
+        Thread::yield();
     }
     
 }
